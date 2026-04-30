374b07d3b08995b47717342b068d4b158fdc178f
JmcaDev <jmcavzla@gmail.com>
2026-04-30 12:12:33 -0400

    JSON & GSON

---
+++ 08-json-con-gson/src/main/java/com/devtalles/proyecto/Main.java
@@ -0,0 +1,23 @@
+package com.devtalles.proyecto;
+
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person("Juan", 25, true);
+
+        Gson gson = new Gson();
+
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+
+        json = "{\"name\":\"Jose\",\"age\":25,\"student\":false}";
+
+        Person person2 = gson.fromJson(json, Person.class);
+
+        System.out.println(person2.toString());
+
+    }
+}
